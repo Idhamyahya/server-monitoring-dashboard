4275198a7baf36c5aa6ac10091fd9050aee16700
idhamyahya <idhamyahya72@gmail.com>
2026-09-17 13:27:30 +0700

    chore: ignore environment files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>VPS Monitoring Dashboard</h1>
+
+      <p>Monitoring VPS dari browser.</p>
+    </div>
+  );
+}
+
+export default App;
